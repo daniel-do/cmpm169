ca20662119a5bf73f4d9f45d4be886b23a4776c5
Daniel Do <do2daniel@yahoo.com>
2024-01-22 23:52:59 -0800

    Finished experiment 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,99 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Daniel Do
+// Date: 01-22-2024
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+// Declare variables to store slider instances, rose radius, and rotation speed
+let sliders;
+let roseRadius;
+let rotationSpeed;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // Create canvas with the size of the window
+  createCanvas(windowWidth, windowHeight);
+  
+  // Initialize variables
+  sliders = [new SliderRose(width / 2, height / 2)]; // Create an array with a new SliderRose at the center
+  rotationSpeed = 1; // Set the initial rotation speed
+  roseRadius = random(60, 200); // Set the initial rose radius
+  background(random(0, 255), random(0, 255), random(0, 255)); // Set a random background color
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  // Update and draw each slider in the sliders array
+  sliders.forEach(d => d.update());
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  // Change the background color to a random value
+  background(random(0, 255), random(0, 255), random(0, 255));
+
+  // Move the first slider in the array with the mouse position as the new origin
+  sliders[0].moveWithMouse(mouseX, mouseY);
+
+  // Set a new random rose radius and rotation speed
+  roseRadius = random(60, 200);
+  rotationSpeed = random(1, 10);
+}
+
+class SliderRose {
+  constructor(_x, _y) {
+    // Initialize SliderRose with a given position
+    this.x1 = _x;
+    this.y1 = _y;
+    this.sliders = []; // Array to store individual sliders
+    this.sinAngle = 0; // Initial angle for sine wave motion
+    this.createSliders(); // Create sliders around the rose
+  }
+
+  createSliders() {
+    const skip = 20; // Angle skip between sliders
+    for (let i = 0; i < 360; i += skip) {
+      const sliderAngle = radians(i);
+      const x2 = cos(sliderAngle) * roseRadius;
+      const y2 = sin(sliderAngle) * roseRadius;
+
+      // Create a slider, set its position, width, and rotation
+      const slider = createSlider(0, 255, 50);
+      slider.position(this.x1 + x2, this.y1 + y2);
+      slider.style('width', roseRadius + 'px');
+      slider.style('transform-origin', '0% 0%');
+      slider.style('transform', 'rotate(' + i + 'deg)');
+      this.sliders.push(slider); // Add the slider to the array
+    }
+  }
+
+  update() {
+    let offset = 0;
+
+    // Update each slider's value based on a sine wave
+    for (let i = 0; i < this.sliders.length; i++) {
+      const x = map(sin(this.sinAngle + offset), -1, 1, 0, 255);
+      this.sliders[i].value(x);
+      offset += 0.050;
+    }
+
+    // Rotate and reposition each slider around the center
+    for (let i = 0; i < this.sliders.length; i++) {
+      const sliderAngle = radians(i * 20);
+      const x2 = cos(sliderAngle) * roseRadius;
+      const y2 = sin(sliderAngle) * roseRadius;
+      this.sliders[i].position(this.x1 + x2, this.y1 + y2);
+      this.sliders[i].style('transform', 'rotate(' + (i * 20 + frameCount * rotationSpeed) + 'deg)');
+    }
+
+    this.sinAngle += 0.1; // Increment the sine wave angle
+  }
+
+  moveWithMouse(mouseX, mouseY) {
+    // Move the entire set of sliders with the mouse position as the new origin
+    this.x1 = mouseX;
+    this.y1 = mouseY;
+
+    for (let i = 0; i < this.sliders.length; i++) {
+      const sliderAngle = radians(i * 20);
+      const x2 = cos(sliderAngle) * roseRadius;
+      const y2 = sin(sliderAngle) * roseRadius;
+      this.sliders[i].position(this.x1 + x2, this.y1 + y2);
+    }
+  }
 }
