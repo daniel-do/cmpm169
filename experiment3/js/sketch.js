927eb7b57c7260cfaa4d82b77b7f63c91a69d369
Daniel Do <do2daniel@yahoo.com>
2024-01-29 19:22:32 -0800

    Added helpful comments in code, removed redundants

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,71 +1,82 @@
-var tileCount = 10;
+// sketch.js - generative methods
+// Author: Daniel Do
+// Date: 01-29-2024
 
-var tileWidth;
-var tileHeight;
-var shapeSize = 100;
-var newShapeSize = shapeSize;
-var shapeAngle = 0;
-var maxDist;
-var currentShape;
-var shapes;
-var canvasContainer;
+// Define variables for tile count, dimensions, fish size, angle, and image container
+let tileCount = 10;
+let tileWidth, tileHeight;
+let fishSize = 100;
+let newFishSize = fishSize;
+let fishAngle = 0;
+let maxDist;
+let currentFish;
+let fishImages = [];
+let canvasContainer;
 
-var sizeMode = 0;
+// Define variable for controlling fish size behavior
+let sizeMode = 0;
 
+// Preload fish image before setup
 function preload() {
-  shapes = [];
-  shapes.push(loadImage('../img/Fish1.png'));
+  fishImages.push(loadImage('../img/Fish1.png'));
 }
 
 function setup() {
+  // Set up canvas and attach it to the specified container
   canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
+  createCanvas(canvasContainer.width(), canvasContainer.height()).parent("canvas-container");
 
-  $(window).resize(function () {
-    resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  });
-  imageMode(CENTER);
-  // set the current shape to the first in the array
-  currentShape = shapes[0];
+  // Adjust canvas size when window is resized
+  $(window).resize(() => resizeCanvas(canvasContainer.width(), canvasContainer.height()));
+
+  // Set initial fish image and calculate tile dimensions
+  currentFish = fishImages[0];
   tileWidth = width / tileCount;
   tileHeight = height / tileCount;
+
+  // Calculate maximum distance for movement speed calculation
   maxDist = sqrt(pow(width, 2) + pow(height, 2));
 }
 
 function draw() {
-    clear();
-    background('#009dc4');
-  
-    for (var gridY = 0; gridY < tileCount; gridY++) {
-      for (var gridX = 0; gridX < tileCount; gridX++) {
-  
-        var posX = tileWidth * gridX + tileWidth / 2;
-        var posY = tileHeight * gridY + tileWidth / 2;
-  
-        // calculate angle between mouse position and actual position of the shape
-        var angle = atan2(mouseY - posY, mouseX - posX) + (shapeAngle * (PI / 180));
-  
-        // calculate distance between mouse and shape
-        var distance = dist(mouseX, mouseY, posX, posY);
-  
-        // calculate movement speed based on distance
-        var speed = 100;
-  
-        // update position towards mouse
-        posX += cos(angle) * speed;
-        posY += sin(angle) * speed;
-  
-        if (sizeMode == 0) newShapeSize = shapeSize;
-        if (sizeMode == 1) newShapeSize = shapeSize * 1.5 - map(distance, 0, 500, 5, shapeSize);
-        if (sizeMode == 2) newShapeSize = map(distance, 0, 500, 5, shapeSize);
-  
-        push();
-        translate(posX, posY);
-        rotate(angle);
-        noStroke();
-        image(currentShape, 0, 0, newShapeSize, newShapeSize);
-        pop();
-      }
+  // Clear canvas and set background color
+  clear();
+  background('#009dc4');
+
+  // Nested loop to iterate through the grid of fish positions
+  for (let gridY = 0; gridY < tileCount; gridY++) {
+    for (let gridX = 0; gridX < tileCount; gridX++) {
+      // Calculate position for each fish in the grid
+      let posX = tileWidth * gridX + tileWidth / 2;
+      let posY = tileHeight * gridY + tileWidth / 2;
+
+      // Calculate angle between fish and mouse position
+      let angle = atan2(mouseY - posY, mouseX - posX) + (fishAngle * (PI / 180));
+      
+      // Calculate distance between mouse and fish
+      let distance = dist(mouseX, mouseY, posX, posY);
+
+      // Set movement speed
+      let speed = 100;
+
+      // Update fish position towards the mouse
+      posX += cos(angle) * speed;
+      posY += sin(angle) * speed;
+
+      // Calculate new fish size based on the selected size mode
+      newFishSize = (sizeMode === 0) ? fishSize : ((sizeMode === 1) ? fishSize * 1.5 - map(distance, 0, 500, 5, fishSize) : map(distance, 0, 500, 5, fishSize));
+
+      // Apply transformations to draw the fish
+      push();
+      translate(posX, posY);
+      rotate(angle);
+      noStroke();
+      image(currentFish, 0, 0, newFishSize, newFishSize);
+      pop();
     }
   }
+}
+
+
+
+
